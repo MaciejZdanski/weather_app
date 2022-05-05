85bc72307fe8231feadd9c1dc0e37597a9d03c09
MaciejZdanski <zdanski.maciej@gmail.com>
2022-05-05 18:04:14 +0200

    a

--- README.md
+++ README.md
@@ -22,3 +22,28 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+
+# WEATHER_APP (VUE API PROJECT)
+## Instalacja
+  0. Jeśli to pierwszy projekt VUE na komputerze - zainstaluj VUE CLI-SERVICE i VUE CLI BABEL
+    npm i -g @vue/cli
+    npm i -g @vue/cli-service
+  1. Stwórz nowy projekt
+    vue create NAZWA-PROJEKTU
+  lub
+    vue create .
+  (jeśli wcześniej utworzyłeś folder projektu)
+
+    * Jeśli wyskoczy błąd "execution of scripts is disabled on this system.", wpisz w POWERSHELL:
+      Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser
+
+  2. Włącz serwer deweloperski:
+    vue serve
+
+##
+1. STWÓRZ BRANCH gh-pages (lewy dolny róg - master ) => create new branch from => gh-pages => master
+2. Wyślij nową gałąź na gh (source control => message => okejka => Sync Changes)
+3. Kliknij add file => Upload file => przenieś pliki z /dist do "Drag and drop files here". => Commit Changes
+4. 
--- src/components/WeatherView.vue
+++ src/components/WeatherView.vue
@@ -1,97 +1,100 @@
 <template>
-    <div class="weatherView">
-        <h1>Weather</h1>
-        <div class="cityPicker">
-            <input type="text" v-model="cityName">
-            <button @click="getLocation">SZUKAJ</button>
-        </div>
-        <div class="currentWeather">
-            <h2>CURRENT WEATHER</h2>
-            <WeatherSingle :current="current" />
-        </div>
-        <div class="dailyWeather">
-            <h2>DAILY WEATHER</h2>
-            <div class="dailyWrapper">
-                <div class="daily" v-for="day in daily" :key="day.dt">
-                    <WeatherSingle :current="day"/>
-                </div>
-            </div>
-        </div>
+  <div class="weatherView">
+    <h1>Weather</h1>
+    <div class="cityPicker">
+      <input type="text" v-model="cityName">
+      <button @click="getLocation">SZUKAJ</button>
     </div>
+
+    <div class="currentWeather">
+      <h2>CURRENT WEATHER</h2>
+      <WeatherSingle :current="current" />
+    </div>
+
+    <div class="dailyWeather">
+      <h2>DAILY WEATHER</h2>
+      <div class="dailyWrapper">        
+        <div class="daily" v-for="day in daily" :key="day.dt">
+          <WeatherSingle :current="day" />
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 import WeatherSingle from './WeatherSingle.vue'
 export default {
-        name: 'WeatherView',
-        components: {
-        WeatherSingle: WeatherSingle
-    },
-    data(){
-        return{
-        cityName: "Katowice",
-        lat: "",
-        lon: "",
-        current: {},
-        daily: [],
-        currentCity: "",
-        }
-    },
-    methods:{
-        getWeather(lat, lon){
-            fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&units=metric&appid=04677c6a3c533e48a61efafe88c2c5ef")
-            .then(dt => dt.json())
-            .then(dt => {
-                console.log(dt)
-                this.daily = dt.daily;
-                this.current = dt.current;
-            })
-        },
-        getLocation(){
-            fetch("http://api.openweathermap.org/geo/1.0/direct?q="+this.cityName+"&appid=04677c6a3c533e48a61efafe88c2c5ef")
-            .then(dt => dt.json())
-            .then(dt => {
-                this.cityName = dt[0].name;
-                this.currentCity = dt[0].name;
-                this.lat = dt[0].lat;
-                this.lon = dt[0].lon;
-                this.getWeather(dt[0].lat, dt[0].lon);
-            })
-        }
-    },
-    created(){
-        this.getLocation();
+  name: 'WeatherView',
+  components: {
+    WeatherSingle: WeatherSingle
+  },
+  data(){
+    return{
+      cityName: "Katowice",
+      lat: "",
+      lon: "",
+      current: {},
+      daily: [],
+      currentCity: "",
     }
+  },
+  methods:{
+    getWeather(lat, lon){
+      fetch("https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&units=metric&appid=04677c6a3c533e48a61efafe88c2c5ef")
+      .then(dt => dt.json())
+      .then(dt => {
+        console.log(dt)
+        this.daily = dt.daily;
+        this.current = dt.current;
+      })
+    },
+    getLocation(){
+      fetch("https://api.openweathermap.org/geo/1.0/direct?q="+this.cityName+"&appid=04677c6a3c533e48a61efafe88c2c5ef")
+      .then(dt => dt.json())
+      .then(dt => {
+        this.cityName = dt[0].name;
+        this.currentCity = dt[0].name;
+        this.lat = dt[0].lat;
+        this.lon = dt[0].lon;
+        this.getWeather(dt[0].lat, dt[0].lon);
+      })
+    }
+  },
+  created(){
+    this.getLocation();
+  }
 }
 </script>
 
 <style lang="scss">
-    .weatherView{
-        .cityPicker{
-        display:flex;
-        justify-content: center;
-        input{
-            padding:.02em;
-            font-size:1.5em;
-            // border:none;
-        }
-        button{
-            padding:.5em;
-            font-size:2em;
-            border:none;
-            background: linear-gradient(#777, #222);
-            color:#fff;
-            border-radius:.5px;
-        }
-        }
-        .currentWeather{
-        flex-direction: column;
-        align-items: center;
-        }
-        .dailyWrapper,.currentWeather{
-        display:flex;
-        justify-content: center;
-        flex-wrap:wrap;
-        }
+  .weatherView{
+    .cityPicker{
+      display:flex;
+      justify-content: center;
+      input{
+        padding:.5em;
+        font-size:2em;
+        // border:none;
+      }
+      button{
+        padding:.5em;
+        font-size:2em;
+        border:none;
+        background: linear-gradient(#777, #222);
+        color:#fff;
+        border-radius:5px;
+      }
     }
+    .currentWeather{
+      flex-direction: column;
+      align-items: center;
+    }
+    .dailyWrapper,.currentWeather{
+      display:flex;
+      justify-content: center;
+      flex-wrap:wrap;
+    }
+  }
 </style>
